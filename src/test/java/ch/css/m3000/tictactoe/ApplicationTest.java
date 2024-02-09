11e1f92ff8a439a51c9abe8de4d04bcda475d2fd
Kuerm <kuerm@gmx.net>
2024-02-09 14:43:32 +0100

    As there will be many very small commits, I don't spend time on commit comments

--- src/test/java/ch/css/m3000/tictactoe/ApplicationTest.java
+++ src/test/java/ch/css/m3000/tictactoe/ApplicationTest.java
@@ -1,4 +1,12 @@
 package ch.css.m3000.tictactoe;
 
-public class ApplicationTest {
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ApplicationTest {
+    @Test
+    void startGameThenEmpty3x3BoardIsReturned() {
+        assertThat(3).isEqualTo(3);
+    }
 }
