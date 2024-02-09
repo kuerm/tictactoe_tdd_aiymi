1a3d6acc46d886e620e3833ceb571f8b13c648ce
Kuerm <kuerm@gmx.net>
2024-02-09 14:47:16 +0100

    3

--- src/test/java/ch/css/m3000/tictactoe/ApplicationTest.java
+++ src/test/java/ch/css/m3000/tictactoe/ApplicationTest.java
@@ -7,8 +7,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ApplicationTest {
     @Test
     void startGameThenEmpty3x3BoardIsReturned() {
-        int[][] board = new int[3][3];
-        
+        int size = 3;
+        int[][] board = new int[size][size];
+
         int actualWidth = board.length;
 
         int expectedWidth = 3;
