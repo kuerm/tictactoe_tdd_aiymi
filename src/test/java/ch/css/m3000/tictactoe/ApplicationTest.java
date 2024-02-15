7ea112cf0c3f2fce4993e75a39a66229f0339c74
Kuerm <kuerm@gmx.net>
2024-02-15 17:35:05 +0100

    32

--- src/test/java/ch/css/m3000/tictactoe/ApplicationTest.java
+++ src/test/java/ch/css/m3000/tictactoe/ApplicationTest.java
@@ -1,12 +1,7 @@
 package ch.css.m3000.tictactoe;
 
-import org.apache.commons.lang3.builder.ToStringBuilder;
-import org.apache.commons.lang3.builder.ToStringStyle;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.Arrays;
-import java.util.Objects;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -97,79 +92,6 @@ class ApplicationTest {
                 assertThat(board.fieldAt(x, y).state()).isEqualTo(FieldState.EMPTY);
             }
         }
-    }
-
-    public static final class Board {
-        private static FieldState currentPlayer;
-        private final FieldState[][] value;
-
-        private Board(FieldState[][] value) {
-            this.value = value;
-        }
-
-        public static Board of(int size) {
-            currentPlayer = FieldState.Y;
-            FieldState[][] fieldStates = new FieldState[size][size];
-            for (FieldState[] fieldState : fieldStates) {
-                Arrays.fill(fieldState, FieldState.EMPTY);
-            }
-            return new Board(fieldStates);
-        }
-
-        private static FieldState nextFieldState() {
-            if (currentPlayer == FieldState.X) {
-                currentPlayer = FieldState.Y;
-                return currentPlayer;
-            }
-            currentPlayer = FieldState.X;
-            return currentPlayer;
-        }
-
-        public int size() {
-            return value.length;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (obj == this) {
-                return true;
-            }
-            if (obj == null || obj.getClass() != this.getClass()) {
-                return false;
-            }
-            var that = (Board) obj;
-            return Arrays.deepEquals(this.value, that.value);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash((Object) value);
-        }
-
-        @Override
-        public String toString() {
-            return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
-        }
-
-        public void play(int x, int y) {
-            if (fieldAt(x, y).state() != FieldState.EMPTY) {
-                return;
-            }
-            setFieldState(x, y, nextFieldState());
-        }
-
-        private void setFieldState(int x, int y, FieldState fieldState) {
-            value[x - 1][y - 1] = fieldState;
-        }
-
-        public Field fieldAt(int x, int y) {
-            return new Field(x, y, fieldStateAt(x, y));
-        }
-
-        private FieldState fieldStateAt(int x, int y) {
-            return value[x - 1][y - 1];
-        }
-
     }
 
 }
